5571d65a63366a0c6b197c456ca8f486534462c8
nekoama <rtanmail2@gmail.com>
2021-03-06 16:10:07 -0800

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ import logo from "../images/nekolistLogo.png"
 function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark sticky">
+            <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark sticky">
                 <Link to="/"><img className="logo" src={logo} alt="logo"></img></Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <Link to="/" class="nav-item nav-link active">Home<span class="sr-only">(current)</span></Link>
-                        <Link to="/anime" class="nav-item nav-link">Anime</Link>
-                        <Link to="/manga" class="nav-item nav-link">Manga</Link>
-                        <Link to="/favorite" class="nav-item nav-link">Favorite</Link>
-                        <Link to="/stats" class="nav-item nav-link">Stats</Link>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link to="/" className="nav-item nav-link active">Home<span className="sr-only">(current)</span></Link>
+                        <Link to="/anime" className="nav-item nav-link">Anime</Link>
+                        <Link to="/manga" className="nav-item nav-link">Manga</Link>
+                        <Link to="/favorite" className="nav-item nav-link">Favorite</Link>
+                        <Link to="/stats" className="nav-item nav-link">Stats</Link>
                     </div>
                 </div>
             </nav>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
+import logo from "../images/nekolistLogo.png"
+
 
 function Home() {
     return (
         <div className="row home-content">
             <div className="col-12 home-hero">
-                <p>Welcome to <span>NekoList!</span></p>
-                <button type="button" class="btn btn-primary">Get Started</button>
+                <p>Welcome to <br></br><img src={logo} alt="logo"></img></p>
+                <Link to="/section2"><button type="button" class="btn btn-primary">Get Started</button></Link>
             </div>
 
-            <div className="col-6 home-info">
+            <div className="col-6 home-info" id="section2">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse ullam, veritatis porro placeat ipsa, dolores quasi magni quis perferendis inventore ipsum hic sed reiciendis impedit nam tempora possimus, dolorum fugiat? Odit, soluta voluptatum ipsa tempora sequi dolore? Mollitia, eum nihil. Alias similique dolorum perspiciatis deleniti corporis facilis vitae tempora, ut quae voluptatibus tenetur excepturi exercitationem modi iure ab quos voluptas inventore nam id odio dicta veniam repellendus molestias enim. Nulla commodi veritatis porro voluptatem laudantium nobis recusandae ea odit minus quo libero architecto ullam iure impedit dolore, dolorum corrupti inventore tempora itaque nesciunt eos? Veritatis esse distinctio quod accusantium, rem delectus harum laudantium reprehenderit et maxime temporibus autem asperiores provident! Obcaecati ea recusandae, autem magni rerum vel omnis quidem!</p>
             </div>
 
